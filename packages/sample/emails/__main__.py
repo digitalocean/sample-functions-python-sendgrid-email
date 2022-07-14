bce63fa156a1d43ef749611771edf427337eaad8
Shivani Kumar <shivanik1920@gmail.com>
2022-07-14 17:21:56 -0400

    Adding comments

--- packages/sample/emails/__main__.py
+++ packages/sample/emails/__main__.py
@@ -3,6 +3,16 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 def main(args):
+    '''
+    Takes in the email address, subject, and message to send an email using SendGrid, 
+    returns a json response letting the user know if the email sent or failed to send.
+
+        Parameters:
+            args: Contains the from email address, to email address, subject and message to send
+
+        Returns:
+            json body: Json response if the email sent successfully or if an error happened
+    '''
     key = os.getenv('API_KEY')
     user_from = args.get("from")
     user_to = args.get("to")
